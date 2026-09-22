34eafe122abc0457b30c0eadd8b5c27d04845721
Ola <oladimejiagbaje144@gmail.com>
2026-09-22 14:43:41 +0100

    update

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const http = require("http");
 const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerDefinition = require("./config/swagger");
 
 const app = express();
@@ -50,7 +51,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
+const swaggerSpec = swaggerJSDoc(swaggerDefinition);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get("/api-docs.json", (req, res) => {
   res.json(swaggerDefinition);
 });
